4bbc16b0d2107a5052a2ede245fd480b9d420a68
tylermorrisford <tylerford@gmail.com>
2019-11-05 23:50:59 -0700

    modal working to view/save notes

--- public/app.js
+++ public/app.js
@@ -1,12 +1,19 @@
+// init sidenav
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.modal').modal();
+  });
+
+
 // Grab the articles as a json
 $.getJSON("/articles", function(data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
-      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+      $("#articles").append("<p class='modal-trigger' data-target='modal1' data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
     }
   });
-  
+//   modal-trigger" data-target='modal1'
   
   // Whenever someone clicks a p tag
   $(document).on("click", "p", function() {
@@ -26,9 +33,9 @@ $.getJSON("/articles", function(data) {
         // The title of the article
         $("#notes").append("<h2>" + data.title + "</h2>");
         // An input to enter a new title
-        $("#notes").append("<input id='titleinput' name='title' >");
+        $("#notes").append("<input id='titleinput' placeholder='note title' name='title' >");
         // A textarea to add a new note body
-        $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+        $("#notes").append("<textarea id='bodyinput' placeholder='note body' name='body'></textarea>");
         // A button to submit a new note, with the id of the article saved to it
         $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
   
