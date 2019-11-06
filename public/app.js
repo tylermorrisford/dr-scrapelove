dd833b238bd58a932b98f2083ff6cf1cfb364f3d
tylermorrisford <tylerford@gmail.com>
2019-11-06 00:06:21 -0700

    add/update style

--- public/app.js
+++ public/app.js
@@ -31,13 +31,13 @@ $.getJSON("/articles", function(data) {
       .then(function(data) {
         console.log(data);
         // The title of the article
-        $("#notes").append("<h2>" + data.title + "</h2>");
+        $("#notes").append("<h5>" + data.title + "</h5>");
         // An input to enter a new title
         $("#notes").append("<input id='titleinput' placeholder='note title' name='title' >");
         // A textarea to add a new note body
         $("#notes").append("<textarea id='bodyinput' placeholder='note body' name='body'></textarea>");
         // A button to submit a new note, with the id of the article saved to it
-        $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+        $("#notes").append("<button class='waves-effect waves-light btn-small black white-text' data-id='" + data._id + "' id='savenote'>Save Note</button>");
   
         // If there's a note in the article
         if (data.note) {
