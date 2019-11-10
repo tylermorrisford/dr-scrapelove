ce796f7e83b6fe48af64ac671f418359150d8049
tylermorrisford <tylerford@gmail.com>
2019-11-10 00:23:45 -0700

    update mongo connection for heroku, add procfile

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // connect to the Mongo DB
-mongoose.connect("mongodb://localhost/freeCodeCampNews", { useNewUrlParser: true, useUnifiedTopology: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/freeCodeCampNews";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Routes================= //
 
